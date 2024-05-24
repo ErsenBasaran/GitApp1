4f5b5c21276d6dd238aaa33f4f9942d253d2099d
Ersen Başaran <ersenbasaran11@gmail.com>
2024-05-24 22:26:42 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,16 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        // versiyon 1
+        System.out.println("helloGIT");
+        System.out.println("git add . TÜM komutları staging areaya alır ");
+        // terminale git add . yazdım. komutları staging area'ya almış oldum
+
+        // terminale git status yazınca hangi komut working space'te hangisi staging area'da hangisi commit'te gösterir
+        // terminalde bu noktada kırmızı klasör görünüyorsa onu da staging area'ya almak için şunu yaparız:
+        // terminale git add ve dosyanın adını uzantısıyla yazarız
+        // örnek : git add src/main/java/Runner.java
+
+
+    }
+}
